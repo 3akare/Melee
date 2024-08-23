8ed4c6949a5a65fa8404e31d38746f98165763d3
3akare <bakare@systemspecscomng.onmicrosoft.com>
2024-08-23 19:28:49 +0100

    feat: read emails and store them in a log file

---
+++ backend/src/main/java/config/PropertiesConfig.java
@@ -0,0 +1,47 @@
+package config;
+
+public class PropertiesConfig {
+    private String password;
+    private String host;
+    private String storeType;
+    private String address;
+
+    public PropertiesConfig(String password, String host, String storeType, String address){
+        this.password = password;
+        this.storeType = storeType;
+        this.host = host;
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getStoreType() {
+        return storeType;
+    }
+
+    public void setStoreType(String storeType) {
+        this.storeType = storeType;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ backend/src/main/java/constant/MeleeConstants.java
@@ -0,0 +1,7 @@
+package constant;
+
+public class MeleeConstants {
+    public MeleeConstants(){}
+
+    public static final Integer BATCH_SIZE = 100;
+}
