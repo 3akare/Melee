bd4c4c0ea1f2b429a24c69841d482afbe4505569
3akare <bakare@systemspecscomng.onmicrosoft.com>
2024-08-26 16:42:42 +0100

    refactor: setup python server

--- parser-server/main.py
+++ parser-server/main.py
@@ -1,15 +1,11 @@
 from flask import Flask, request, jsonify
-from transformers import LongformerTokenizer, LongformerForSequenceClassification
-import torch
 
 app = Flask(__name__)
 
-tokenizer = LongformerTokenizer.from_pretrained('allenai/longformer-base-4096')
-model = LongformerForSequenceClassification.from_pretrained('allenai/longformer-base-4096')
-
 @app.route('/process', methods=['POST'])
 def process_text():
-    return jsonify(request)
+    payload = request.get_data().decode()
+    return str(payload)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5500")
+    app.run(host="0.0.0.0", port="5500", debug=True)
